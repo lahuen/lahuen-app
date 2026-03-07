33bc67c170f200048e593ebe2829a2d2c077889e
maxflorentin <macsee13@gmail.com>
2026-03-07 11:22:21 -0300

    fix ERR_QUIC_PROTOCOL_ERROR

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,5 +15,6 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
+  experimentalForceLongPolling: true,
 });
 export const googleProvider = new GoogleAuthProvider();
